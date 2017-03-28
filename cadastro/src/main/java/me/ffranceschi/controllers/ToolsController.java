369495c3e91d98e7298684929d0f7263bbf96378
Fernando Franceschi <fernando.franceschi@sigma.com.br>
2017-03-28 00:43:27 -0300

    hystrix dashboard

--- cadastro/src/main/java/me/ffranceschi/controllers/ToolsController.java
+++ cadastro/src/main/java/me/ffranceschi/controllers/ToolsController.java
@@ -5,6 +5,7 @@ package me.ffranceschi.controllers;
  */
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
--- processo/src/main/java/me/ffranceschi/ProcessoApplication.java
+++ processo/src/main/java/me/ffranceschi/ProcessoApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableAutoConfiguration
+@EnableHystrixDashboard
 public class ProcessoApplication {
 
 	public static void main(String[] args) {
